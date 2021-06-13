a2908705cd19c202854e28d9cd961ef4e8a4920a
Israel Deorce <israel.idvj@gmail.com>
2021-06-13 17:17:46 -0300

    📦️ Updating eslint rules and pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    'jest/globals': true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style'
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -15,7 +18,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'jest'
   ],
   settings: {
     react: {
