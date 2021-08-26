ad57215d348def12c0c9c4da31f20c640e29c47c
Israel Deorce <israel.idvj@gmail.com>
2021-08-25 21:48:12 -0300

    📦️ Added jest-styled-components package

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import 'jest-styled-components'
 
 global.shallow = shallow
 global.render = render
